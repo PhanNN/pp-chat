5980b48b5efde1a5a1e084d29aa9d758109352e6
Pi <nguyengocphan@gmail.com>
2019-03-25 16:23:56 +0700

    _:change to use beautiful girs :3

--- app/controllers/contactController.js
+++ app/controllers/contactController.js
@@ -3,19 +3,19 @@ exports.getContacts = (req, res, user) => {
     'docs': [
       {
           'name': 'Demo',
-          'avatar': 'https://s3-us-west-2.amazonaws.com/s.cdpn.io/382994/thomas.jpg'
+          'avatar': 'https://i-h2.pinimg.com/564x/3d/d0/6c/3dd06c3d0e5dd47e0ee16f1da7babcd8.jpg'
       },
       {
           'name': 'Demo1',
-          'avatar': 'https://s3-us-west-2.amazonaws.com/s.cdpn.io/382994/dog.png'
+          'avatar': 'https://i-h2.pinimg.com/564x/00/97/1b/00971b1a4b0e1fd3b369cfc14b3f5a13.jpg'
       },
       {
           'name': 'Demo2',
-          'avatar': 'https://s3-us-west-2.amazonaws.com/s.cdpn.io/382994/louis-ck.jpeg'
+          'avatar': 'https://i-h2.pinimg.com/564x/dc/c9/fb/dcc9fbffb32abdab0e51dad25140999c.jpg'
       },
       {
           'name': 'Bingo',
-          'avatar': 'https://s3-us-west-2.amazonaws.com/s.cdpn.io/382994/bo-jackson.jpg'
+          'avatar': 'https://i-h2.pinimg.com/564x/43/c4/66/43c466ae4055c7d2baea56c1adfc9403.jpg'
       }
     ]
   }});
